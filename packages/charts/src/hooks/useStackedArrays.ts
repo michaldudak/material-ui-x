b9f8c04f77a98f1d62cf1adb10b57025285de667
Michał Dudak <michal@dudak.me>
2021-09-24 17:41:33 +0100

    [utils] Convert useStackedArrays to Typescript

--- packages/charts/src/hooks/useStackedArrays.ts
+++ packages/charts/src/hooks/useStackedArrays.ts
@@ -1,6 +1,6 @@
-import { useMemo } from 'react';
+import * as React from 'react';
 
-function sumArrays(...arrays) {
+function sumArrays(...arrays: number[][]) {
   const n = arrays.reduce((max, xs) => Math.max(max, xs.length), 0);
   const result = Array.from({ length: n });
   return result.map((_, i) => arrays.map((xs) => xs[i] || 0).reduce((sum, x) => sum + x), 0);
@@ -10,17 +10,19 @@ function sumArrays(...arrays) {
 // of the preceding array's corresponding elements
 // [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 // => [[1, 2, 3], [5, 7, 9], [12, 15, 18]]
-function useStackedArrays(arrays) {
-  return useMemo(() => {
+function useStackedArrays(arrays: number[]): number[];
+function useStackedArrays(arrays: number[][]): number[][];
+function useStackedArrays(arrays: number[] | number[][]) {
+  return React.useMemo(() => {
     if (Array.isArray(arrays[0])) {
-      return arrays.reduce((acc, curr) => {
+      return (arrays as number[][]).reduce((acc, curr) => {
         if (acc.length > 0) {
           acc.push(sumArrays(acc[acc.length - 1], curr));
         } else {
           acc.push(curr);
         }
         return acc;
-      }, []);
+      }, [] as number[][]);
     }
     return arrays;
   }, [arrays]);
